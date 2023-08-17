7a8a965bc2d76c1db4e151377e29b947916cd8b8
Miguel <ingecorrea@gmail.com>
2023-08-17 18:57:05 -0300

    Agregar archivos de proyecto.

---
+++ PrimerProyecto/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PrimerProyecto
+{
+    partial class PrimerForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHolaMundo = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.Aceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHolaMundo
+            // 
+            this.lblHolaMundo.AutoSize = true;
+            this.lblHolaMundo.Location = new System.Drawing.Point(19, 149);
+            this.lblHolaMundo.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.lblHolaMundo.Name = "lblHolaMundo";
+            this.lblHolaMundo.Size = new System.Drawing.Size(284, 54);
+            this.lblHolaMundo.TabIndex = 0;
+            this.lblHolaMundo.Text = "Hola Mundo";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(40, 55);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(213, 34);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // Aceptar
+            // 
+            this.Aceptar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Aceptar.Location = new System.Drawing.Point(301, 69);
+            this.Aceptar.Name = "Aceptar";
+            this.Aceptar.Size = new System.Drawing.Size(114, 47);
+            this.Aceptar.TabIndex = 2;
+            this.Aceptar.Text = "Aceptar";
+            this.Aceptar.UseVisualStyleBackColor = true;
+            this.Aceptar.Click += new System.EventHandler(this.Aceptar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(28F, 54F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 275);
+            this.Controls.Add(this.Aceptar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblHolaMundo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHolaMundo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button Aceptar;
+    }
+}
+
---
+++ PrimerProyecto/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimerProyecto
+{
+    public partial class PrimerForm : Form
+    {
+        public PrimerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Aceptar_Click(object sender, EventArgs e)
+        {
+            lblHolaMundo.Text = "Hola " + txtNombre.Text;
+        }
+    }
+}
